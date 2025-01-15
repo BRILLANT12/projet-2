05107f698473b5f831c2b297a61bff340a5c4eac
remifoures <remifoures@icloud.com>
2025-01-15 20:30:39 +0100

    .

--- src/main/java/com/example/br/HelloController.java
+++ src/main/java/com/example/br/HelloController.java
@@ -1,14 +1,57 @@
-package com.example.br;
+package org.example.pzpzp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    @FXML private Button valider;
+    @FXML private Button exite;
+    @FXML private PasswordField nom;
+    @FXML private PasswordField identifient;
+    @FXML private Text titre;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void valider(ActionEvent actionEvent) {
+        // Action du bouton Valider
+        Button.setOnAction(event -> {
+            if (identifient.contains(IDCHEK)){
+                navigateBasedOnID(nom, identifient);
+            } else {
+                showAlert("Erreur", "Veuillez entrer un ID valide.");
+            }
+        });
+
+        // Configuration de la fenêtre principale
+        Stage primaryStage = null;
+        primaryStage.setTitle("Navigation par ID");
+        primaryStage.setScene(loginScen);
+        primaryStage.show();
     }
+
+    // Méthode pour naviguer en fonction de l'ID
+    private void navigateBasedOnID(PasswordField nom, PasswordField userId) {
+        if (userId.equals("000")) {
+            showManagerPage(nom);
+        } else if (userId.equals("001")) {
+            showEmployeePage(nom);
+        } else {
+            showAlert("Erreur", "ID inconnu.");
+        }
+    }
+
+    private void showAlert(String erreur, String s) {
+    }
+
+    // Méthode pour afficher la page du Manager
+    private void showManagerPage(Stage stage) {
+    }
+    // Méthode pour afficher la page de l'Employé
+    private void showEmployeePage(Stage stage) {
+    }
+
 }
+
+
