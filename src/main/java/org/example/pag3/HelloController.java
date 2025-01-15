7bdaa88449ea3d3632caa50371b28000b124d099
remifoures <remifoures@icloud.com>
2025-01-15 20:37:39 +0100

    h

---
+++ src/main/java/org/example/pag3/HelloController.java
@@ -0,0 +1,25 @@
+package org.example.pag3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+public class HelloController {
+    @FXML private Button exite;
+    @FXML private Button fini;
+    @FXML private Text texte;
+    @FXML private TableView tableau;
+    @FXML private TextField textee;
+    @FXML private TextField exofin;
+    @FXML private TableColumn un;
+    @FXML private TableColumn deux;
+
+    public void exite(ActionEvent actionEvent) {
+    }
+
+    public void fini(ActionEvent actionEvent) {
+    }
+
+
+}
