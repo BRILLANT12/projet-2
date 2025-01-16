fce528afa859b62340d2cca4bfec6dbd61bbab6c
remifoures <remifoures@icloud.com>
2025-01-16 11:21:38 +0100

    h

--- src/main/java/org/example/pag3/HelloController.java
+++ src/main/java/org/example/pag3/HelloController.java
@@ -3,6 +3,7 @@ package org.example.pag3;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class HelloController {
@@ -22,4 +23,6 @@ public class HelloController {
     }
 
 
+    public void nom(MouseEvent mouseEvent) {
+    }
 }
